3e29e7a6c462e95be8a65aba3a30a740fcec50e9
Ale14p12 <alejandronicmen@gmail.com>
2026-02-16 11:33:35 -0600

    Archivos de java

---
+++ Public/js/register.js
@@ -0,0 +1,14 @@
+const fullname = document.getElementById("fullname")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const confirmpassword = document.getElementById("confirmpassword")
+const registerbutton = document.getElementById("registerbutton")
+
+
+
+
+
+
+async function registrarUsuario() {
+    
+}
